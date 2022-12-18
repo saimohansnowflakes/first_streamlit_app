0fb06f964030e4453440d057340a8aeb1a6e0794
Saimohan <120618988+saimohansnowflakes@users.noreply.github.com>
2022-12-19 00:38:01 +0530

    Update streamlit_app.py
    moved stop function to below

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+
 
 
 
@@ -53,7 +53,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
