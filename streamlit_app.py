8af78c43bf308cd57a0e12ab7fffa4b2fc56e1b5
Saimohan <120618988+saimohansnowflakes@users.noreply.github.com>
2022-12-19 00:10:20 +0530

    Update streamlit_app.py
    correct variable name for my_cur from mycur

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-mycur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
