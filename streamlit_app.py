6fb2f96d0be33fd587b36eff383d5abc0adc6ca6
Saimohan <120618988+saimohansnowflakes@users.noreply.github.com>
2022-12-18 20:40:54 +0530

    Update streamlit_app.py
    added kiwi and normalized the json response into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
+
+# take the json version of response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
